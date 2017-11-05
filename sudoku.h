dbd28a5918dd645266491877a6850b0d45ae6456
Rathod  Prasad Vitthal <rathodpv16.comp@coep.ac.in>
2017-11-05 11:52:13 +0000

    Replace sudoku.h

--- sudoku.h
+++ sudoku.h
@@ -1,15 +1,20 @@
-typedef struct node {
+typedef struct Sudoku {
 	int puzzle[25][25];
 	int type;
 	/*For any N*N sudoku, here 'type' denotes 'N' i.e order of sudoku*/
-}node;
-typedef node* sudoku;
-int countzero(sudoku);
-int sudokuSolver(sudoku,int row,int col);
-int isvalid(int,sudoku,int row, int col);
-int inrow(sudoku,int row,int num);
-int incol(sudoku,int row,int num);
-int inbox(sudoku,int row,int col,int num);
-void generate(sudoku);
-int iscorrect(sudoku);
-void print(sudoku);
+} Sudoku;
+
+void init();
+int verify();
+void show();
+void savef();
+void loadfile();
+void Diagonal_box(int row,int col);
+int fillit(int row, int col);
+int sudokuSolver(int row,int col);
+int isvalid(int row, int col, int num);
+int inrow(int row,int col,int num);
+int incol(int row, int col, int num);
+int square(int row,int col,int num);
+void fill_box(int row,int col);
+void helpMe();
